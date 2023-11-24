6e9e9b337c9417bc1ed7111c4866e1c6d5ec5e14
Fabricio IF <fabricio.bizotto@ifc.edu.br>
2023-11-24 17:18:58 -0300

    asd

--- 05-vetor/exemplo-1.c
+++ 05-vetor/exemplo-1.c
@@ -9,7 +9,14 @@ int main(void) {
     scanf("%d", &vetor[3]);
     scanf("%d", &vetor[4]);
 
+    printf("\nOs valores digitados foram:\n");
     printf("%d\n", vetor[0]);
+    printf("%d\n", vetor[1]);
+    printf("%d\n", vetor[2]);
+    printf("%d\n", vetor[3]);
+    printf("%d\n", vetor[4]);
+    // printf("%d\n", vetor[5]); // Acessa um índice inválido
+
 
     return 0;
 }
